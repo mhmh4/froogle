3ac5ae97bec9ca6715381aff3b12386693fefa42
miah <86537561+mhmh4@users.noreply.github.com>
2023-04-29 14:19:58 -0400

    Update app.py

--- public/backend/app.py
+++ public/backend/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 openai.organization = "YOUR_ORG_ID"
 # openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
 openai.Model.list()
 
 openai.Completion.create(
