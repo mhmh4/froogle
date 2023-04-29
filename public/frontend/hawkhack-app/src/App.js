06d726e56912556295fa21b82c9057307283145d
Chrissie Zimmer <czimmer12@aol.com>
2023-04-29 14:53:05 -0400

    Frontend react skeleton

---
+++ public/frontend/hawkhack-app/src/App.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import { useState } from "react";
+
+import './App.css';
+
+const defaultFoods = [{
+  name: "Apple",
+  calories: "65",
+  tags: ["Crisp", "crunchy", "sweet", ]
+},
+{
+  name: "Chicken",
+  calories: "120",
+  tags: ["artsy"]
+}, {
+  name: "Rice",
+  calories: "60",
+  tags: ["shrek"]
+}];
+
+const projectMapper = (project) => {
+  const projectStyle = {
+    backgroundColor: "rgba(255,200,200,50)",
+    width: "50%",
+    marginLeft: "25%",
+    borderRadius: 10
+  }
+  return (<>
+    <br></br>
+    <div style={projectStyle}>
+      <p style={{ fontWeight: "bold", color:"red" }}>{project.name}</p>
+      <p>{project.calories}</p>
+    </div>
+  </>)
+}
+
+function App() {
+  const [projects, setProjects] = useState([...defaultFoods]);
+  const [name, setName] = useState("");
+  const [calories, setCalories] = useState("");
+
+  return (
+    <div className="App">
+      <p>Foods</p>
+      {projects.map(projectMapper)}
+
+      <div>
+        <input type="text" placeholder="Name" style={{ width: "80%" }}
+          onChange={(event) => { setName(event.target.value); }}/>
+        <br></br>
+        <br></br>
+        <input type="text" placeholder="Calories" style={{ width: "80%" }}
+        onChange={(event) => { setCalories(event.target.value); }}/>
+        <br></br>
+        <br></br>
+        <button style={{ fontSize: 20 }} onClick={() => {
+          let _projects = [...projects];
+
+          _projects.push({
+            name: name,
+            calories: calories
+          })
+          setProjects(_projects)
+        }}>Add Foods</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
