774f11861e57d92faaafc610825b0faf6a25a4a3
miah <86537561+mhmh4@users.noreply.github.com>
2023-04-29 14:19:37 -0400

    Update app.py

--- public/backend/app.py
+++ public/backend/app.py
@@ -5,14 +5,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-openai.organization = "YOUR_ORG_ID"
+openai.organization = "org-j4zwDScvVmoK6QRr70fOQ42Z"
 # openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
+openai.api_key = "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"
 openai.Model.list()
 
 openai.Completion.create(
   model="text-davinci-003",
   prompt="Say this is a test",
-  max_tokens=7,
-  temperature=0
+  # max_tokens=7,
+  # temperature=0
 )
