dedb9ea2a5e894f7a86727683f7ae83318ea51a5
Zak-bond <111093537+Zak-bond@users.noreply.github.com>
2023-04-29 18:45:15 -0400

    styling

--- public/frontend/hawkhack-app/src/App.js
+++ public/frontend/hawkhack-app/src/App.js
@@ -19,9 +19,11 @@ const router = createBrowserRouter([
     element: (
       <div>
       <h1>Froogle</h1>
-      <Link to="/about"> About Us</Link>
-      <Link to="/mainapp"> Mainapp</Link>
-      <Link to="/placeholder">Placeholder</Link>
+      <div>
+      <Link to="/about" class="Nav-links"> About Us</Link>
+      <Link to="/mainapp" class="Nav-links"> Mainapp</Link>
+      <Link to="/placeholder" class="Nav-links">Placeholder</Link>
+      </div>
     </div>
     ),
     
@@ -105,8 +107,16 @@ function App() {
       <div>
         <RouterProvider router={router} />
       </div>
+<<<<<<< Updated upstream
 
         <input type="text" placeholder="Name" style={{ width: "40%", height: 15}}
+=======
+    
+        <p className="food" id="food">Foods</p>
+        {projects.map(projectMapper)}
+        <div>
+        <input type="text" placeholder="Name" style={{ width: "80%" }}
+>>>>>>> Stashed changes
           onChange={(event) => { setName(event.target.value); }}/>
         <div style={{ display: "flex", gridTemplateColumns: 'repeat(3, 1fr)' }}>
           {projects.map(projectMapper)}
