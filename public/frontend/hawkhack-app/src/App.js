1b57d3c78ce3e78d54756e7b80554eaf432029df
miah <86537561+mhmh4@users.noreply.github.com>
2023-04-30 07:43:45 -0400

    Update App.js

--- public/frontend/hawkhack-app/src/App.js
+++ public/frontend/hawkhack-app/src/App.js
@@ -157,11 +157,11 @@ const Project = ({ project }) => {
                     <img className='foodImg' style={{ left: 400, top: 20, rotate: '-80deg' }} src="https://em-content.zobj.net/thumbs/120/google/350/avocado_1f951.png"/>
                     <h1 className="logo">froogle</h1>
                     <img className='foodImg' style={{ right: 350, top: 1, rotate: '40deg' }} src="https://em-content.zobj.net/thumbs/120/apple/354/strawberry_1f353.png"/>
-                    <img className='foodImg' style={{ right: 430, top: 40, rotate: '30deg' }} src="https://em-content.zobj.net/thumbs/120/apple/354/bell-pepper_1fad1.png"/>
+                    <img className='foodImg' style={{ right: 410, top: 40, rotate: '30deg' }} src="https://em-content.zobj.net/thumbs/120/apple/354/bell-pepper_1fad1.png"/>
                     <img className='foodImg' style={{ right: 343, top: 55 }} src="https://em-content.zobj.net/thumbs/120/google/350/watermelon_1f349.png"/>
                 <img className='foodImg' style={{ left: 330, top: 50 }} src="https://em-content.zobj.net/thumbs/120/apple/354/mango_1f96d.png"/>
 
-                {/* <img className='foodImg' style={{ left: 20, top: 155 }} src="https://em-content.zobj.net/thumbs/120/apple/354/bread_1f35e.png"/> */}
+                <img className='foodImg' style={{ left: 330, top: 5, rotate: '-30deg' }} src="https://em-content.zobj.net/thumbs/120/apple/354/bread_1f35e.png"/>
 
                     <input className="search" style={{ 
 marginBottom: '30px',
