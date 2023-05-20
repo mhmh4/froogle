4808cfe3a0f624b54ca3e97878405fdc6daf9198
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-05-20 19:01:18 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
-import requests
 
 import config
 
@@ -11,25 +10,6 @@ app.config["CORS_HEADERS"] = "Content-Type"
 
 
 openai.api_key = config.OPENAI_API_KEY
-
-
-def get_image_url(query):
-    url = "https://www.googleapis.com/customsearch/v1"
-    params = {
-        "key": config.GOOGLE_KEY,
-        "cx": config.GOOGLE_CX,
-        "q": f"{query}",
-        "searchType": "image",
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        response_json = response.json()
-        if "items" in response_json:
-            results = response_json["items"]
-            if results:
-                return results[0]["link"]
-
-    return None
 
 
 def call_chatgpt_api(input):
@@ -44,15 +24,7 @@ def call_chatgpt_api(input):
     recipe_names= recipe_names.split("\n")
     for i in range(len(recipe_names)):
         recipe_names[i] = recipe_names[i][3:]
-    # print(recipe_names)
-    image_urls = []
-    for recipe in recipe_names:
-        image_urls.append(get_image_url(recipe))
-    data = []
-    for a, b in zip(recipe_names, image_urls):
-        data.append([a, b])
-    print(data)
-    return data
+    return recipe_names
 
 
 @app.route("/recipes")
