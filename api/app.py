2485799f3644762946bebba327f20ba0ee8985ba
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-05-21 06:55:28 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,39 +1,36 @@
 import os
 
 import dotenv
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+import flask
+import flask_cors
 import openai
 
-app = Flask(__name__)
-cors = CORS(app)
+app = flask.Flask(__name__)
 app.config["CORS_HEADERS"] = "Content-Type"
+
+cors = flask_cors.CORS(app)
 
 dotenv.load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def call_chatgpt_api(input):
+def call_openai_api(input):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": "I'll give ingredients, give me recipes, just list the recipe names as a numerical list, don't output anything else"},
-            {"role": "user", "content": f"Here are the ingredients: {input}. List recipes, just list the recipe names, nothin else"},
+            {"role": "user", "content": f"Here are the ingredients: {input}. List recipes, just list the recipe names, nothing else"},
         ]
     )
     recipe_names = completion.choices[0].message.content
-    recipe_names= recipe_names.split("\n")
+    recipe_names = recipe_names.split("\n")
     for i in range(len(recipe_names)):
         recipe_names[i] = recipe_names[i][3:]
     return recipe_names
 
 
-@app.route("/recipes")
-def generate_recipes():
-    query_string = str(request.query_string)
-    response = call_chatgpt_api(query_string)
-    return jsonify(response)
-
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/recipes", methods=["GET"])
+def recipes():
+    query_string = str(flask.request.query_string)
+    response = call_openai_api(query_string)
+    return flask.jsonify(response)
